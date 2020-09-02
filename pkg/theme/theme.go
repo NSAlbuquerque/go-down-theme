0e6a6dc7d17c7b3c3a7dcb53dc844cab94e70643
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-09-02 16:16:05 -0300

    Refactor theme models

--- pkg/theme/theme.go
+++ pkg/theme/theme.go
@@ -1,12 +1,25 @@
 package theme
 
-// ThemeMetadata metainformações sobre temas
-type ThemeMetadata struct {
-	Name        string `json:"name"`
-	URL         string `json:"url"`
-	Light       bool   `json:"light"`
-	ProjectRepo string `json:"projectRepo"`
+import (
+	"io"
+	"time"
+)
+
+// Theme metainformações sobre temas
+type Theme struct {
+	Name        string    `json:"name"`
+	URL         string    `json:"url"`
+	Light       bool      `json:"light"`
+	ProjectRepo string    `json:"projectRepo"`
+	Version     string    `json:"version,omitempty"`
+	UpdatedAt   time.Time `json:"updatedAt,omitempty"`
 }
 
 // Gallery representa um conjunto de metainformações sobre temas.
-type Gallery []ThemeMetadata
+type Gallery []Theme
+
+// Provider disponibiliza metainformações sobre temas a partir de uma fonte.
+type Provider interface {
+	GetGallery() (Gallery, error)
+	Download(url string, to io.Writer) error
+}
