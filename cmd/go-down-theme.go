aed3b9164de8d9825b4489dc46288d72e79e51a5
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-09-02 16:57:58 -0300

    Remove unnecessary variable

--- cmd/go-down-theme.go
+++ cmd/go-down-theme.go
@@ -9,13 +9,11 @@ var (
 	dirArg string
 )
 
-const galleryURL = "https://tmtheme-editor.herokuapp.com/gallery.json"
-
 func init() {
 	flag.StringVar(&dirArg, "to", ".", "Diretório destino")
 	flag.Parse()
 }
 
 func main() {
-	log.Println(dirArg, galleryURL)
+	log.Println(dirArg)
 }
