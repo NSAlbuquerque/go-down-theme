380861244d1eebfa2c75397e9a9c087f60154b99
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-09-03 03:41:53 -0300

    Remove download method from provider

--- pkg/theme/theme.go
+++ pkg/theme/theme.go
@@ -1,7 +1,6 @@
 package theme
 
 import (
-	"io"
 	"time"
 )
 
@@ -24,5 +23,4 @@ type Gallery []Theme
 // Provider disponibiliza metainformações sobre temas a partir de uma fonte.
 type Provider interface {
 	GetGallery() (Gallery, error)
-	Download(url string, to io.Writer) error
 }
