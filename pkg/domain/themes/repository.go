01ffef9255c75a139683a263a36fe2e7ac036a57
Natan Albuquerque <natan.albuquerque@stone.com.br>
2020-12-10 00:17:32 -0300

    feat: add filter param to themes.Repository.List

--- pkg/domain/themes/repository.go
+++ pkg/domain/themes/repository.go
@@ -7,6 +7,12 @@ type Repository interface {
 	Save(context.Context, *Theme) error
 	SaveGallery(context.Context, Gallery) error
 	Get(context.Context, string) (*Theme, error)
-	List(context.Context) (Gallery, error)
+	List(context.Context, *ListFilter) (Gallery, error)
 	Delete(context.Context, string) error
+}
+
+// ListFilter filter querys.
+type ListFilter struct {
+	Limit  int
+	Offset int
 }
